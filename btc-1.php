f326b9f69c95dcae2bf57cc84a37448136471d0a
syco101 <142594949+syco101@users.noreply.github.com>
2024-05-24 17:02:42 +0200

    Add files via upload

---
+++ btc-1.php
@@ -0,0 +1,106 @@
+<!DOCTYPE php>
+<php lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bitcoin</title>
+
+
+</head>
+<body>
+
+
+ <!-- Website auf https://bitcoinchart.w3spaces.com/index.html ausgeführt -->
+
+
+    <h1>Bitcoin Live Chart</h1>
+
+ <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf1L5FpeDzjRayjRLzU94KwIjqrWU4vo1YqusoJGzWA&s" 
+ height=200px width=200px>
+ 
+    <!-- TradingView Widget BEGIN -->
+    <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+    <script type="text/javascript">
+        new TradingView.widget(
+            {
+            "width": 1900,
+            "height": 650,
+            "symbol": "BINANCE:BTCUSDT",
+            "interval": "D",
+            "timezone": "Etc/UTC",
+            "theme": "light",
+            "style": "1",
+            "locale": "de",
+            "toolbar_bg": "#f1f3f6",
+            "enable_publishing": false,
+            "hide_top_toolbar": true,
+            "save_image": false,
+            "container_id": "tradingview_b45de"
+            }
+        );
+    </script>
+    <div id="tradingview_b45de"></div>
+
+    <br><br><br><br>
+    <p1> Bitcoin ist eine dezentrale
+         digitale Währung, die 2009 von einer unbekannten Person oder Gruppe 
+         unter dem Pseudonym Satoshi Nakamoto
+        eingeführt wurde. Sie basiert auf einer 
+        Technologie namens Blockchain, die Transaktionen
+         in einer öffentlichen, verteilten Datenbank aufzeichnet.</p1>
+<br> 
+<br>
+
+         <p1> 
+            
+            Merkmale
+
+            
+         </p1> 
+            <p1> 
+                 Dezentralisierung:
+
+            <br><br>
+
+            Bitcoin wird nicht von einer
+             zentralen Institution wie einer Bank oder Regierung kontrolliert.
+             Stattdessen wird das Netzwerk von einer Vielzahl von Computern,
+             sogenannten Nodes, betrieben, die weltweit verteilt sind.
+
+             
+            </p1> 
+            <p1> 
+             Blockckchain-Technologie:
+
+             <br><br>
+
+            Die Blockchain ist ein öffentliches, unveränderliches Register, das alle Transaktionen speichert.
+            Neue Transaktionen werden in Blöcken zusammengefasst und chronologisch angeordnet.
+            Jeder Block enthält eine Referenz zum vorherigen Block,
+            wodurch eine Kette entsteht. Dies gewährleistet die Integrität und Sicherheit der Transaktionshistorie.
+
+            
+        </p1>
+
+        <p1> 
+            Transparenz und Anonymität:
+
+            <br><br>
+
+            Transaktionen sind transparent, da sie in der Blockchain öffentlich einsehbar sind. 
+            Dennoch bleibt die Identität der Nutzer anonym, da sie durch kryptografische Adressen (Wallets) repräsentiert werden.
+
+           
+
+        </p1>
+        <p1> 
+            Limitierte Gesamtmenge:
+
+            <br><br>
+
+            Die maximale Anzahl an Bitcoins ist auf 21 Millionen begrenzt.
+             Diese Begrenzung soll Inflation vorbeugen.
+            
+            </p1>
+</body>
+</php>
